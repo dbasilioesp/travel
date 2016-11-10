d931dea5440863356f76fdec8c6238fcffc854cc
David Basilio <dbasilioesp@gmail.com>
2016-11-10 14:45:19 -0200

    Preprocessors styles with livereload.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,54 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    
     watch: {
-      all: {
-        files: ["*.html"],
-        options: {
-          livereload: true
+        livereload: {
+          options: { livereload: true },
+          files: ['*.html', 'sass/*.scss'],
+        },
+        sass: {
+            files: ["sass/*.scss"],  
+            tasks: ['sass', 'postcss'],
         }
-      }
     },
 
     connect: {
         server: {
           options: {
             port: 9001,
-            open: true
           }
         }
-      }
+    },
+
+    sass: {
+        dist: {
+            options: {
+                style: 'compressed',
+                sourcemap: 'auto',
+                lineNumbers: true
+            },
+            files: {
+                'css/app.css': 'sass/app.scss'
+            }
+        }    
+    },
+
+    postcss: {
+        options: {
+            map: true,
+            processors: [require('autoprefixer')({browsers: 'last 4 versions'})]
+        },
+        dist: {
+            src: 'css/app.css'
+        }
+    }
+
   });
 
-  grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-sass");
   
   grunt.registerTask("default", ["connect", "watch"]);
 };
